7a8d1ddd008e274e07a34da59bf8a0238177997d
IsoMine <door2space@gmail.com>
2018-09-22 16:08:57 +0300

    Add project files.

---
+++ ChatWebApplication/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            if (!(string.IsNullOrEmpty(name)) && !(string.IsNullOrEmpty(message))) {
+                Clients.All.addNewMessageToPage(name, message, DateTime.Now.ToShortTimeString());
+            }
+        }
+    }
+}
